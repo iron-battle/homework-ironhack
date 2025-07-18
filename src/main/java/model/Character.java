8fc3271d0fca110fd931fbc06eccafb24e52e3d8
FelixRodriguezG <correofrg@gmail.com>
2025-07-18 13:31:40 +0200

    Abstract class Character has been created

--- src/main/java/model/Character.java
+++ src/main/java/model/Character.java
@@ -1,4 +1,47 @@
 package model;
 
-public class Character {
+public abstract class Character {
+    private String id;
+    private String name;
+    private int hp;
+    private boolean isAlive;
+
+    public Character(String id, String name, int hp) {
+        this.id = id;
+        this.name = name;
+        this.hp = hp;
+        this.isAlive = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
